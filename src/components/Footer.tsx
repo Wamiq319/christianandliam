35c3a5bbe8c0941f3d4570a0f0cdc40fcd723785
Wamiq <wamiqzahid319@gmail.com>
2025-07-18 03:46:05 +0500

    Footer Added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
+import { FaInstagram, FaFacebook } from "react-icons/fa";
+
 export default function Footer() {
   return (
     <footer className="bg-gray-100 py-10 px-6 md:px-12">
@@ -11,17 +13,17 @@ export default function Footer() {
             href="https://www.instagram.com/cg87mommy"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-[#397d0c]"
+            className="hover:text-[#397d0c] flex items-center cursor-pointer"
           >
-            Instagram
+            <FaInstagram className="mr-1" /> Instagram
           </a>
           <a
             href="https://www.facebook.com/krissy.michelle.568"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-[#397d0c]"
+            className="hover:text-[#397d0c] flex items-center cursor-pointer"
           >
-            Facebook
+            <FaFacebook className="mr-1" /> Facebook
           </a>
           <a
             href="https://www.gofundme.com/f/help-liam-get-a-new-accessible-vehicle"
